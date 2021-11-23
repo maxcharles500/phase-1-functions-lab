6ed2a85c4e557c2555e020897c1d59b14a51fbd0
maximilianjones <maxcharles500@gmail.com>
2021-11-22 18:10:19 -0800

    commit tree 8831af969f830e44ed5fc64f21bd0b4d74859883

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp) {
+   if (pickUp >= 42) {
+    return pickUp - 42;
+    
+   }
+   else if (pickUp < 42) {
+       return  42 - pickUp;
+       
+   }
+}
+
+function distanceFromHqInFeet(pickUp) {
+    return distanceFromHqInBlocks(pickUp) * 264;
+    
+  }
+  function distanceTravelledInFeet(start, destination) {
+   if (start >= destination) {
+       return (start - destination) * 264;
+   }
+   else if (start < destination) {
+       return (destination - start) * 264;
+   }
+  }
+
+  function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400){
+        return 0;
+    }
+    else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * 2.0 / 100;
+    }
+    else if (distanceTravelledInFeet(start, destination) >= 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25
+    }
+    else {
+        return 'cannot travel that far'
+    }
+
+  }
